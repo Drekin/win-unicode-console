1528228f2b1d2aeb29b6b371248acf9134103998
Drekin <drekin@gmail.com>
2015-08-10 16:54:39 +0200

    Version 0.4-post
    The version number in `setup.py` was changed.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*relative_path_parts):
 	with open(path.join(HERE, *relative_path_parts), encoding=ENCODING) as f:
 		return f.read()
 
-VERSION = "0.4"
+VERSION = "0.4-post"
 README = "README.rst"
 URL = "https://github.com/Drekin/win-unicode-console"
 
